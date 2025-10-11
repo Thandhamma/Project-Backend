e7561ebf07504736f5f5287c37a719a544ff7092
Banana <thandhamma1505@gmail.com>
2025-10-11 18:32:07 +0700

    backend update 11/10/2568 18:32PM

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';    
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI ?? throw new Error('MONGO_URI environment variable is not defined')),
+    ProductsModule,
+    AuthModule,
+
+  ],
+})
+export class AppModule {}
